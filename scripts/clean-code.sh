cee317caad89cde0f4f59bbafb1290c4c9e147de
Matthew Grasmick <matt.grasmick@gmail.com>
2020-02-24 14:21:53 -0500

    Upgrading to min requirment of Symfony 4.

--- scripts/clean-code.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-./vendor/bin/phpcbf --standard=PSR2 src --exclude=Generic.Files.LineLength
-./vendor/bin/phpcbf --standard=PSR2 tests/src --exclude=Generic.Files.LineLength
-./vendor/bin/phpcbf --standard=PSR2 bin --exclude=Generic.Files.LineLength
